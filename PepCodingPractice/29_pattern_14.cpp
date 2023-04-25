46f6580bec4722f9d206915e1e2af04df2e5510f
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2023-04-25 22:05:06 +0530

    add pattern number table

---
+++ PepCodingPractice/29_pattern_14.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int 
+main() {
+    int num = 2;
+
+    for (int i = 1; i <=10; i++) {
+        std::cout << num <<  "*"  << i << " = " << num*i << std::endl;
+    }
+    
+
+    return 0;
+
+}
