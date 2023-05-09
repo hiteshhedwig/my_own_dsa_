87c0741620c3f6c25638947e40491f0a087dd356
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2023-05-09 15:01:28 +0530

    update optimal sol

--- Striver/left_rotate_d.cpp
+++ Striver/left_rotate_d.cpp
@@ -11,13 +11,27 @@ int main() {
     int left_rotate  = 3 ; 
     int size_arr = 7;
 
-    for (int j = 0 ; j < left_rotate ; j++) {
-        int temp = arr[0] ;
-        for (int i = 1 ; i < size_arr ; i++) { 
-            arr[i-1] = arr[i] ;
-        }
-        arr[size_arr-1] = temp ;
+    // for (int j = 0 ; j < left_rotate ; j++) {
+    //     int temp = arr[0] ;
+    //     for (int i = 1 ; i < size_arr ; i++) { 
+    //         arr[i-1] = arr[i] ;
+    //     }
+    //     arr[size_arr-1] = temp ;
+    // }
+    // rotate left
+    int temp[left_rotate] ;
+    for (int i = 0 ; i < left_rotate ; i++) { 
+        temp[i] = arr[i] ;
     }
+
+    for (int i = 0 ; i < size_arr ; i++) {
+        arr[i] = arr[left_rotate+i] ;
+    }
+
+    for (int i = 0 ; i < left_rotate ; i++) { 
+        arr[(left_rotate+i + 1)] = temp[i] ;
+    }
+
 
     for (size_t i = 0; i < size_arr; i++) {
         std::cout << arr[i] << " ";
