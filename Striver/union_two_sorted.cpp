e83ec98f9b0a4dc8000d62d744c02de87a2cbdd3
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2023-05-09 20:58:48 +0530

    update striver result

--- Striver/union_two_sorted.cpp
+++ Striver/union_two_sorted.cpp
@@ -1,27 +1,49 @@
-// Union of Two Sorted Arrays
 
-#include<bits/stdc++.h>
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector < int > FindUnion(int arr1[], int arr2[], int n, int m) {
+  int i = 0, j = 0; // pointers
+  vector < int > Union; // Uninon vector
+  while (i < n && j < m) {
+    if (arr1[i] <= arr2[j]) // Case 1 and 2
+    {
+      if (Union.size() == 0 || Union.back() != arr1[i])
+        Union.push_back(arr1[i]);
+      i++;
+    } else // case 3
+    {
+      if (Union.size() == 0 || Union.back() != arr2[j])
+        Union.push_back(arr2[j]);
+      j++;
+    }
+  }
+  while (i < n) // IF any element left in arr1
+  {
+    if (Union.back() != arr1[i])
+      Union.push_back(arr1[i]);
+    i++;
+  }
+  while (j < m) // If any elements left in arr2
+  {
+    if (Union.back() != arr2[j])
+      Union.push_back(arr2[j]);
+    j++;
+  }
+  return Union;
+}
 
 int main()
+
 {
-    int arr1[] = {1,2,3,4,5};
-    int arr2[] = {2,3,4,4,5};
-    int size_arr = 5;
-
-    // output : {1,2,3,4,5}
-
-    std::set <int> output;
-
-    // nO(logn)
-    for (int i = 0; i < size_arr; i++) {
-        output.insert(arr1[i]);
-        output.insert(arr2[i]);
-    }
-
-    for (auto & val : output) {
-        std::cout << val << " ";
-    }
-    std::cout << std::endl;
-
-    return 0;
+  int n = 10, m = 5;
+  int arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  int arr2[] = {4, 4, 5, 11, 12}; 
+  vector < int > Union = FindUnion(arr1, arr2, n, m);
+  cout << "Union of arr1 and arr2 is  " << endl;
+  for (auto & val: Union)
+    cout << val << " ";
+  return 0;
 }
