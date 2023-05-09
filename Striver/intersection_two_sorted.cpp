e5c4bb86a5dd84c1e7c0ebb59aef5f3508e3a54c
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2023-05-09 20:57:59 +0530

    add comments

--- Striver/intersection_two_sorted.cpp
+++ Striver/intersection_two_sorted.cpp
@@ -11,9 +11,17 @@ int main()
     int idx_2 = 0;
 
     for (int i = 0; i < n; i++) {
+        // keep two pointer track in arr1, arr2
+        // if arr1 value is smaller than arr2, we need to update idx2
+        // why ? becoz when arr1 idx is smaller than arr2 idx2. 
+        // arr2 value is already been recorded.
         if (arr2[idx_2] < arr1[i]) {
             idx_2++;
         } 
+
+        // if val in arr1, and arr2 are same then its intersection
+        // and idx_2 needs to be smaller than arr2 length
+        // once duplicated recorded, update idx_2 and find for next number
         if (idx_2 < m && arr1[i] == arr2[idx_2]) {
             std::cout << "intersection " << arr1[i] << std::endl;
             out.push_back(arr1[i]);
