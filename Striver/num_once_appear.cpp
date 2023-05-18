bd1c7a2b3ce1c6139dd208b7a101364eba9ea1c4
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2023-05-19 00:34:08 +0530

    num once appear

--- Striver/num_once_appear.cpp
+++ Striver/num_once_appear.cpp
@@ -6,20 +6,13 @@ int main()
     int arr[] = {4,1,2,1,2} ;
     int n_size = sizeof(arr)/sizeof(arr[0]);
     
-    // we can use hashmaps
-    std::unordered_map<int, int> uq;
-    for (int i = 0; i < n_size; i++) {
-        uq[arr[i]] += 1;
+    int xorr=0;
+    for (int i=0; i<n_size; i++) {
+        xorr = xorr ^ arr[i];
     }
 
-    int once=0;
-    for (auto x : uq) {
-        if (x.second == 1) {
-            once = x.first;
-        }
-        // uq.insert({arr[i], 0});
-    }
-    std::cout << "once occured " << once << std::endl;
+
+    std::cout << "once occured " << xorr << std::endl;
 
   return 0;
 }
