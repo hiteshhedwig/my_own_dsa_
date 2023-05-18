e1821aaa224c8dd3ad6009744577521b40d29e5f
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2023-05-19 00:33:47 +0530

    ans with recursion

--- Striver/binary_search.cpp
+++ Striver/binary_search.cpp
@@ -20,6 +20,19 @@ void find_num(int *arr, int to_find, int start, int end, int mid) {
     }
 }
 
+int find_recursion(int *arr, int to_find, int start, int end, int mid) {
+    std::cout << arr[start] << " " << arr[end] <<  " "<< arr[mid]<< std::endl;
+    // std::cout << start << " " << end <<  " "<< mid<< std::endl;
+    if (start > end) return -1;
+    mid = (start + end)/2;
+    if (arr[mid] == to_find) { std::cout << "FOUND! returning" << std::endl ; return mid; }
+    if (arr[mid] > to_find) {
+        return find_recursion(arr, to_find, start, mid-1, mid);
+    } else {
+        return find_recursion(arr, to_find, mid+1, end, mid);
+    }
+}
+
 int main()
 {
     int nums[] = {2,3,7,10,13,14,17} ;
@@ -32,8 +45,10 @@ int main()
     int find = 14;
 
     // without recursion:
-    find_num(nums, find, start, end, mid);
+    // find_num(nums, find, start, end, mid);
 
-    
+    // with recursion:
+    auto ans = find_recursion(nums, find, start, end, mid);
+    std::cout << ans << std::endl;
   return 0;
 }
