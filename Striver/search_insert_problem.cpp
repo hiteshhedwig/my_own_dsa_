079b7cc1bc7c5438ea87845b41888f0ebc7d0641
Hitesh Kumar <hitesh.kumar61@yahoo.com>
2023-05-20 22:25:37 +0530

    add search insert problem

---
+++ Striver/search_insert_problem.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int start=0;
+        int end  = nums.size()-1;
+        int mid  = (start+end)/2;
+        while (start < end) {
+            if (nums[mid]==target) {
+                return mid;
+            }
+            if (nums[mid]<target) {
+                start = mid + 1;
+            } else {
+                end   = mid - 1;
+            }
+            mid  = (start+end)/2;
+        }
+
+        if (nums[mid] > target) {
+            return mid;
+        }
+        if (nums[mid] == target) {
+            return mid;
+        }
+
+        return mid+1;
+    }
+};
